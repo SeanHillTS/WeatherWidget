d6d84fda8376830fb7b8cb6e3c4179b8e268f150
Sean Hill <sean@tangentsolutions.co.za>
2017-06-28 13:30:12 +0200

    OnInit and pipes

--- app/weather-widget/component/weather.component.ts
+++ app/weather-widget/component/weather.component.ts
@@ -1,6 +1,6 @@
 import { Weather } from './../model/weather';
 import { WeatherService } from './../service/weather.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'weather-widget',
@@ -11,28 +11,40 @@ import { Component } from '@angular/core';
     providers: [WeatherService]
 })
 
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
     pos: Position;
     weatherData = new Weather(null, null, null, null, null);
 
     constructor(private service: WeatherService) {
+
+    }
+
+    ngOnInit() {
+        this.getCurrentLocation();
+    }
+
+    getCurrentLocation() {
         this.service.getCurrentLocation().subscribe(pos => {
             this.pos = pos;
-
-            this.service.getCurrentWeather(pos.coords.latitude, pos.coords.longitude)
-                .subscribe(
-                weather => {
-                    console.log(weather);
-                },
-                err => {
-                    console.error(err);
-                });
-
+            this.getCurrentWeather()
         },
             err => {
                 console.error(err);
             });
+    }
 
-
+    getCurrentWeather() {
+        this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
+            .subscribe(
+            weather => {
+                this.weatherData.temp = weather["currently"]["temperature"];
+                this.weatherData.summary = weather["currently"]["summary"];
+                this.weatherData.wind = weather["currently"]["windSpeed"];
+                this.weatherData.humidity = weather["currently"]["humidity"];
+                this.weatherData.icon = weather["currently"]["icon"];
+            },
+            err => {
+                console.error(err);
+            });
     }
 }
