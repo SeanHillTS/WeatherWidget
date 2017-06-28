c35f1cc65a54e234757edd8209043882ff3ece01
Sean Hill <sean@tangentsolutions.co.za>
2017-06-28 11:09:20 +0200

    Added weatherService

--- app/weather-widget/component/weather.component.ts
+++ app/weather-widget/component/weather.component.ts
@@ -1,12 +1,17 @@
+import { WeatherService } from './../service/weather.service';
 import { Component } from '@angular/core';
-
 @Component({
     moduleId : module.id,
     selector: 'weather-widget',
     templateUrl: 'weather.component.html',
     styleUrls:[
         'weather.component.css'
-    ]
+    ],
+    providers: [WeatherService]
 })
 
-export class WeatherComponent{}
+export class WeatherComponent{
+    constructor(private service: WeatherService){
+
+    }
+}
